faf6f22ab2ac746e317c5b5d3cc5a5406c2c1a9e
vicbiz <vicbizmoon@hotmail.com>
2018-04-20 13:38:35 -0400

    React Weather App

---
+++ src/components/Titles.js
@@ -0,0 +1,22 @@
+import React from "react";
+
+// class Title extends React.Component{
+//     render(){
+//         return (
+//             <div>
+//                 <h1>Weather Finder</h1>
+//                 <p>Find out temperature, conditions and more...</p>
+//             </div>
+//         );
+//     }
+// };
+
+// Stateless coding..
+const Title = () => (
+    <div>
+        <h1 className="title-container__title">Weather Finder</h1>
+        <h3 className="title-container__subtitle">Find out temperature, conditions and more...</h3>
+    </div>
+);
+
+export default Title;
